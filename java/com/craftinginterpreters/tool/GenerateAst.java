587ed2b4ac285651a79da433c935eca5c3bf1d3c
Omar Bassam <omar.bassam88@gmail.com>
2025-02-23 15:16:44 +0200

    Inheritance. End of Java Implementation

--- java/com/craftinginterpreters/tool/GenerateAst.java
+++ java/com/craftinginterpreters/tool/GenerateAst.java
@@ -19,6 +19,7 @@ class GenerateAst {
             "Literal  : Object value",
             "Logical  : Expr left, Token operator, Expr right",
             "Set      : Expr object, Token name, Expr value",
+            "Super    : Token keyword, Token method",
             "This     : Token keyword",
             "Unary    : Token operator, Expr right",
             "Call     : Expr callee, Token paren, List<Expr> arguments",
@@ -29,7 +30,8 @@ class GenerateAst {
 
         defineAst(outputDir, "Stmt", Arrays.asList(
             "Block      : List<Stmt> statements",
-            "Class      : Token name, List<Stmt.Function> methods",
+            "Class      : Token name, Expr.Variable superclass," +
+                        " List<Stmt.Function> methods",
             "Expression : Expr expression",
             "Function   : Token name, List<Token> params," +
                         " List<Stmt> body",
