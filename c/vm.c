0d6c163518d0ca64a598e56b94d56be0f1648a69
Omar Bassam <omar.bassam88@gmail.com>
2026-01-03 13:38:25 +0200

    Compiling Expressions

--- c/vm.c
+++ c/vm.c
@@ -23,41 +23,41 @@ static InterpretResult run() {
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
 #define BINARY_OP(op)                                                          \
   do {                                                                         \
-	double b = pop();                                                          \
-	double a = pop();                                                          \
-	push(a op b);                                                              \
+    double b = pop();                                                          \
+    double a = pop();                                                          \
+    push(a op b);                                                              \
   } while (false)
 
   for (;;) {
 #ifdef DEBUG_TRACE_EXECUTION
-	printf("          ");
-	for (Value* slot = vm.stack; slot < vm.stackTop; slot++) {
-	  printf("[ ");
-	  printValue(*slot);
-	  printf(" ]");
-	}
-	printf("\n");
-	disassembleInstruction(vm.chunk, (int)(vm.ip - vm.chunk->code));
+    printf("          ");
+    for (Value* slot = vm.stack; slot < vm.stackTop; slot++) {
+      printf("[ ");
+      printValue(*slot);
+      printf(" ]");
+    }
+    printf("\n");
+    disassembleInstruction(vm.chunk, (int)(vm.ip - vm.chunk->code));
 #endif
-	uint8_t instruction;
-	switch (instruction = READ_BYTE()) {
-	case OP_CONSTANT: {
-	  Value constant = READ_CONSTANT();
-	  push(constant);
-	  break;
-	}
-	case OP_ADD: BINARY_OP(+); break;
-	case OP_SUBTRACT: BINARY_OP(-); break;
-	case OP_MULTIPLY: BINARY_OP(*); break;
-	case OP_DIVIDE: BINARY_OP(/); break;
-	case OP_NEGATE: push(-pop()); break;
+    uint8_t instruction;
+    switch (instruction = READ_BYTE()) {
+    case OP_CONSTANT: {
+      Value constant = READ_CONSTANT();
+      push(constant);
+      break;
+    }
+    case OP_ADD: BINARY_OP(+); break;
+    case OP_SUBTRACT: BINARY_OP(-); break;
+    case OP_MULTIPLY: BINARY_OP(*); break;
+    case OP_DIVIDE: BINARY_OP(/); break;
+    case OP_NEGATE: push(-pop()); break;
 
-	case OP_RETURN: {
-	  printValue(pop());
-	  printf("\n");
-	  return INTERPRET_OK;
-	}
-	}
+    case OP_RETURN: {
+      printValue(pop());
+      printf("\n");
+      return INTERPRET_OK;
+    }
+    }
   }
 
 #undef READ_BYTE
@@ -66,8 +66,21 @@ static InterpretResult run() {
 }
 
 InterpretResult interpret(const char* source) {
-  compile(source);
-  return INTERPRET_OK;
+  Chunk chunk;
+  initChunk(&chunk);
+
+  if (!compile(source, &chunk)) {
+    freeChunk(&chunk);
+    return INTERPRET_COMPILE_ERROR;
+  }
+
+  vm.chunk = &chunk;
+  vm.ip = vm.chunk->code;
+
+  InterpretResult result = run();
+
+  freeChunk(&chunk);
+  return result;
 }
 
 void push(Value value) {
